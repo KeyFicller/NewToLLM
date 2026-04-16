6bd12dbf83f6bbe584b72c96068d0adc343f69dc
KeyFicller <799872510@qq.com>
2026-04-16 23:47:25 +0800

    torch brief2

--- cpp_impl/my_torch/brief/torch_breif.cpp
+++ cpp_impl/my_torch/brief/torch_breif.cpp
@@ -1,9 +1,33 @@
 #include "my_torch.h"
 
 #include <iostream>
+#include <torch/nn/modules/linear.h>
 #include <torch/torch.h>
 
 namespace MyTorch {
+
+struct NeuralNetworkImpl : public torch::nn::Module {
+public:
+  NeuralNetworkImpl(int num_inputs, int num_outputs)
+      : torch::nn::Module("NeuralNetwork") {
+    layers = register_module(
+        "layers", torch::nn::Sequential(
+                      torch::nn::Linear(num_inputs, 30), torch::nn::ReLU(),
+                      torch::nn::Linear(30, 20), torch::nn::ReLU(),
+                      torch::nn::Linear(20, num_outputs)));
+  }
+
+  torch::Tensor forward(torch::Tensor x) {
+    auto logits = layers->forward(x);
+    return logits;
+  }
+
+public:
+  torch::nn::Sequential layers{nullptr};
+};
+
+TORCH_MODULE(NeuralNetwork);
+
 void brief_torch() {
   // Scalar/Vector/Matrix/Tensor
   torch::Tensor tensor0d = torch::tensor(1);
@@ -23,5 +47,60 @@ void brief_torch() {
   std::cout << "tensorfloat.dtype: " << tensorfloat.dtype() << std::endl;
   torch::Tensor tensorfloat64 = tensorfloat.to(torch::kFloat64);
   std::cout << "tensorfloat64.dtype: " << tensorfloat64.dtype() << std::endl;
+
+  // Basic operations
+  std::cout << "tensor2d: " << tensor2d << std::endl;
+  std::cout << "tensor2d.reshape: "
+            << tensor2d.reshape({tensor2d.size(1), tensor2d.size(0)})
+            << std::endl;
+  std::cout << "tensor2d.view: "
+            << tensor2d.view({tensor2d.size(1), tensor2d.size(0)}) << std::endl;
+  std::cout << "tensor2d.T: " << tensor2d.transpose(0, 1) << std::endl;
+
+  std::cout << "tensor2d.matmul(tensor2d.T): "
+            << tensor2d.matmul(tensor2d.transpose(0, 1)) << std::endl;
+  std::cout << "tensor2d @ tensor2d.T: "
+            << tensor2d.matmul(tensor2d.transpose(0, 1)) << std::endl;
+
+  // Auto grad
+  torch::Tensor a = torch::tensor({1.0});
+  torch::Tensor b = torch::tensor({2.0});
+  torch::Tensor x = torch::tensor({3.0}, torch::requires_grad(true));
+
+  torch::Tensor y = a * x + b;
+  std::cout << "y = a * x + b, a = 1, grad(y, x) "
+            << torch::autograd::grad({y}, {x}, {}, true) << std::endl;
+
+  y.backward();
+  std::cout << "x.grad: " << x.grad() << std::endl;
+
+  // Neural network
+  torch::manual_seed(1234);
+  NeuralNetwork model(50, 3);
+  std::cout << "neural network: " << model << std::endl;
+  int num_params = 0;
+  for (const auto &param : model->parameters()) {
+    num_params += param.numel();
+  }
+  std::cout << "number of parameters: " << num_params << std::endl;
+
+  auto layer0_any = model->layers->ptr(0);
+  auto layer0 = std::dynamic_pointer_cast<torch::nn::LinearImpl>(layer0_any);
+  if (layer0) {
+    std::cout << "weights for layer[0]: " << layer0->weight << std::endl;
+    std::cout << "shape of weights for layer[0]: " << layer0->weight.sizes()
+              << std::endl;
+  }
+
+  torch::Tensor X = torch::rand({1, 50});
+  std::cout << "Neural network input: " << X << std::endl;
+  auto out = model->forward(X);
+  std::cout << "Neural network output with grad: " << out << std::endl;
+
+  {
+    torch::NoGradGuard no_grad;
+    out = torch::softmax(model->forward(X), 1);
+  }
+  std::cout << "Neural network output without grad: " << out << std::endl;
 }
 } // namespace MyTorch
--- python_impl/basic/torch_brief.py
+++ python_impl/basic/torch_brief.py
@@ -1,4 +1,25 @@
 import torch
+import torch.nn.functional as F
+from torch.autograd import grad
+
+class NeuralNetwork(torch.nn.Module):
+    def __init__(self, num_inputs, num_outputs):
+        super().__init__()
+
+        self.layers = torch.nn.Sequential(
+            # First hidden layer
+            torch.nn.Linear(num_inputs, 30),
+            torch.nn.ReLU(),
+            # Second hidden layer
+            torch.nn.Linear(30, 20),
+            torch.nn.ReLU(),
+            # Output layer
+            torch.nn.Linear(20, num_outputs),
+        )
+
+    def forward(self, x):
+        logits = self.layers(x)
+        return logits
 
 def brief_torch():
     # Scalar/Vector/Matrix/Tensor
@@ -18,3 +39,40 @@ def brief_torch():
     print("tensorfloat.dtype: ", tensorfloat.dtype)
     tensorfloat64 = tensorfloat.to(torch.float64)
     print("tensorfloat64.dtype: ", tensorfloat64.dtype)
+
+    # Basic operations
+    print("tensor2d: ", tensor2d)
+    print("tensor2d.reshape: ", tensor2d.reshape(tensor2d.shape[1], tensor2d.shape[0]))
+    print("tensor2d.view: ", tensor2d.view(tensor2d.shape[1], tensor2d.shape[0]))
+    print("tensor2d.T: ", tensor2d.T)
+
+    print("tensor2d.matmul(tensor2d.T): ", tensor2d.matmul(tensor2d.T))
+    print("tensor2d @ tensor2d.T: ", tensor2d @ tensor2d.T)
+
+    # Auto grad
+    a = torch.tensor([1.0])
+    b = torch.tensor([2.0])
+    x = torch.tensor([3.0], requires_grad=True)
+
+    y = a * x + b
+    print("y = a * x + b, a = 1, grad(y, x) ", grad(y, x, retain_graph=True))
+
+    y.backward()
+    print("x.grad: ", x.grad)
+
+    # Neural network
+    torch.manual_seed(1234)
+    model = NeuralNetwork(50, 3)
+    print("neural network: ", model)
+    num_params = sum(p.numel() for p in model.parameters())
+    print("number of parameters: ", num_params)
+    print("weights for layer[0]: ", model.layers[0].weight)
+    print("shape of weights for layer[0]: ", model.layers[0].weight.shape)
+
+    X = torch.rand((1, 50))
+    print("Neural network input: ", X)
+    out = model(X)
+    print("Neural network output with grad: ", out)
+    with torch.no_grad():
+        out = torch.softmax(model(X), dim=1)
+    print("Neural network output without grad: ", out)
